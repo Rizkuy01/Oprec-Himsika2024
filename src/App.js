ae792d249c40196cca5108c48f112a6bbb60ad31
Rizkuy01 <rizkysaputraa2004.com>
2023-07-14 18:01:59 +0700

    Headline Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
+import HeadlineCards from "./component/HeadlineCards";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <HeadlineCards />
     </div>
   );
 }
