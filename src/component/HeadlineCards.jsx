9eb411139ee1291eab98d0aed29abbae64df6e3b
Rizkuy01 <rizkysaputraa2004.com>
2023-07-23 20:18:16 +0700

    updated

--- src/component/HeadlineCards.jsx
+++ src/component/HeadlineCards.jsx
@@ -10,8 +10,8 @@ const HeadlineCards = () => {
             {/* Overlay */}
             <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
               <p className='font-bold text-2xl px-2 pt-4'>Geprek Sriwedari</p>
-              <p className='px-2'>Lorem Ipsum</p>
-              <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+              <p className='px-2'>paket 10K yang menjadi andalan mahasiswa <br /> dalam mengatasi waktu makan siang.</p>
+              {/* <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button> */}
             </div>
             <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://10619-2.s.cdn12.com/rests/original/108_508242611.jpg" alt="soto" />
           </div>
@@ -20,8 +20,8 @@ const HeadlineCards = () => {
             {/* Overlay */}
             <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
               <p className='font-bold text-2xl px-2 pt-4'>Warteg Rizky</p>
-              <p className='px-2'>Lorem Ipsum</p>
-              <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+              <p className='px-2'>Warteg 24 jam yang berlokasi tepat di depan kampus <br />yang menjadikannya warteg paling diminati seluruh mahasiswa.</p>
+              {/* <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button> */}
             </div>
             <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://assets.jatimnetwork.com/crop/0x0:0x0/750x500/webp/photo/2023/01/06/1985907977.jpg" alt="soto" />
           </div>
@@ -30,8 +30,8 @@ const HeadlineCards = () => {
             {/* Overlay */}
             <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
               <p className='font-bold text-2xl px-2 pt-4'>Es Teh Poci</p>
-              <p className='px-2'>Lorem Ipsum</p>
-              <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+              <p className='px-2'>Mempunyai sistem 'Drive Thru'menjadikannya minuman <br />yang praktis dan murah untuk menghilangkan dahaga.</p>
+              {/* <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button> */}
             </div>
             <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://cdn.idntimes.com/content-images/post/20230217/325302538-938170374221710-1614704619464833841-n-05ce5ad9472fcb0af2afa7798a46ec73_600x400.png" alt="soto" />
           </div>
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             {/* Overlay */}
             <div className="absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center">
                 <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span className='text-orange-500'>Best</span></h1>
-                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span className='text-orange-500'>Foods </span>Delivered</h1>
+                <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'><span className='text-orange-500'>Foods </span>Recomendation</h1>
             </div>
             <img className='w-full max-h-[500px] object-cover' src="https://images.unsplash.com/photo-1495130656884-992a9f30d178?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=803D%3D&auto=format&fit=crop&w=2070&q=80" alt="geprek" />
         </div>
